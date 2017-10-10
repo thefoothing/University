72529f0eb2d012e73d2d8b92938e68fc56dabe26
thefoothing <lewisfoo666@gmail.com>
2017-10-10 13:53:58 +0100

    Completed lab 2

--- Comp/Comp/Comp.cpp
+++ Comp/Comp/Comp.cpp
@@ -4,8 +4,18 @@
 #include "stdafx.h"
 
 #include <string>
+#include <iostream>
 struct timeType
 {
+	timeType() {
+	
+	};
+	timeType(int hr, double min, int sec) {
+		this->hr = hr;
+		this->min = min;
+		this->sec = sec;
+
+	}
 		int hr; 
 		double min; 
 		int sec; 
@@ -19,9 +29,19 @@ struct tourType
 };
 
 
+void printTourType(tourType tour) {
+	std::cout << tour.cityName << ' ' << tour.distance << std::endl;
+	std::cout << tour.travelTime.hr << ':' << tour.travelTime.min << ':' << tour.travelTime.sec << std::endl;
+}
+
 int main()
 {
-	
+	tourType destination;
+	destination.cityName = "Nottingham";
+	destination.distance = 130;
+	destination.travelTime = timeType(3,15,0);
+	printTourType(destination);
+	std::cin.get();
     return 0;
 }
 
